b65e2621354d4bae5dc243ef5e1176dd8afc1967
EnzoMartinez01 <enzomartinez2412@gmail.com>
2023-03-09 15:29:51 -0500

    Commitupdate

---
+++ static/js/redirect.js
@@ -0,0 +1,15 @@
+function RedirectLinkedin() {
+    window.location.href="https://www.linkedin.com/in/enzomartinez24/";
+}
+
+function RedirectGithub() {
+    window.location.href="https://github.com/EnzoMartinez01";
+}
+
+function RedirectInstagram() {
+    window.location.href="https://www.instagram.com/iamenzo_01/";
+}
+
+function RedirectCorreo(){
+    window.location.href="mailto:enzomartinez2412@gmail.com";
+}
